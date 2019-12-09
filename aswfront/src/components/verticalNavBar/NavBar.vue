c5ee605a1950ceb81749c4919b1d3edc052c86f1
SergiSC <sergi.serrano-casalins-external@capgemini.com>
2019-12-09 14:00:38 +0100

    dasdsa

--- aswfront/src/components/verticalNavBar/NavBar.vue
+++ aswfront/src/components/verticalNavBar/NavBar.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="vetical-navbar">
         <img class="img-navbar" src="https://icon-library.net/images/bitbucket-icon/bitbucket-icon-8.jpg">
+        <div class="background-img">
+            <img class="img-navbar-home" src="../../assets/house.png" >
+        </div>
+        <div class="background-img">
+            <img class="img-navbar-home" src="../../assets/list.png" >
+        </div>
+        <div class="background-logout-img">
+            <img class="img-navbar-home" src="../../assets/list.png" >
+        </div>
     </div>
 </template>
 
@@ -16,11 +25,36 @@ export default {
     height: 100%;
     background-color: var(--navbar-color);
     position: fixed;
-    top:0px;
+    top:0;
+    left:0;
+}
+.background-img {
+    background-color: white;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    margin: 50px auto 0 auto;
+    z-index: 1;
 }
 .img-navbar {
     width: 50px;
     height: 50px;
     margin-top: 50px;
+}
+.img-navbar-home {
+    position: relative;
+    width: 20px;
+    height: 20px;    
+    margin-top: 15px;
+}
+.background-logout-img {
+    background-color: white;
+    position: absolute;
+    margin: auto;
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    bottom: 50px;
+    z-index: 1;
 }
 </style>
